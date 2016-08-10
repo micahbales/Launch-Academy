5954d36e928f01b04b0bb4f284574e7baa3544d0
Micah Bales <micahbales@gmail.com>
2016-08-10 12:03:31 -0400

    completed vacation_adventure

---
+++ challenges/phase-1/vacation_adventure/code.rb
@@ -0,0 +1,11 @@
+random_choice = rand(3) + 1
+
+if random_choice < 2
+  puts "Let's go jet skiing!"
+elsif random_choice <= 2 && random_choice < 3
+  puts "We should go to the animal reservation"
+elsif random_choice >= 3
+  puts "Let's go sailing!"
+end
+
+  

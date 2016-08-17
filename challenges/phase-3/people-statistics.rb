c36b5a46a421ca8d98359ccc4be4ec73a96f7ddd
Micah Bales <micahbales@gmail.com>
2016-08-17 10:39:38 -0400

    Completed compound-data-structure-drills

---
+++ challenges/phase-3/people-statistics.rb
@@ -0,0 +1,13 @@
+people = [
+  { name: "George Michael Bluth", age: 18, eye_color: "brown", hometown: "Newport Beach" },
+  { name: "Lindsay Bluth Funke",  age: 36, eye_color: "blue",  hometown: "Newport Beach" },
+  { name: "Lucille Bluth",        age: 65, eye_color: "brown", hometown: "Newport Beach" },
+  { name: "Maeby Funke",          age: 17, eye_color: "green", hometown: "Boston" },
+  { name: "GOB Bluth",            age: 38, eye_color: "brown", hometown: "Newport Beach" },
+  { name: "Tobias Funke",         age: 43, eye_color: "brown", hometown: "Boston" }
+]
+
+people_statistics = {
+  age: {twenty_or_younger: 2, over_twenty: 4},
+  eye_color: {brown: 4, blue: 1, green: 1},
+  hometown: {"Newport Beach" => 4, "Boston" => 2}

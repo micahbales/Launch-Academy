3cb4e11d41b5843f75e47d6c7b8397630e6e6f4a
Micah Bales <micahbales@gmail.com>
2016-09-07 14:27:44 -0400

    Completed todo_list

--- challenges/phase-7/todo_list/server.rb
+++ challenges/phase-7/todo_list/server.rb
@@ -5,3 +5,13 @@ set :public_folder, File.join(File.dirname(__FILE__), "public")
 get "/hello" do
   "<p>Hello, world! The current time is #{Time.now}.</p>"
 end
+
+get "/tasks" do
+  @tasks = ["pay bills", "buy milk", "learn Ruby", "get root"]
+  erb :index
+end
+
+get "/tasks/:task_name" do
+  @task_name = params[:task_name]
+  erb :show
+end

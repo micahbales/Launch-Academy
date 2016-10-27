d8d44839c7a5a83e635955ccfe934364e0e46f7e
Micah Bales <micahbales@gmail.com>
2016-10-27 16:03:12 -0400

    completed optimal-guesser

---
+++ challenges/phase-12/optimal-guesser/guess_number.rb
@@ -0,0 +1,16 @@
+def guess_number(min, max)
+
+  while min <= max
+
+    middle = (max + min) / 2
+    guess = check(middle)
+
+    if guess == 1
+      max = middle - 1
+    elsif guess == -1 
+      min = middle + 1
+    else
+      return middle
+    end
+  end
+end

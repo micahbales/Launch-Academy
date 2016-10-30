a971d81e8823d1751151519180cc1087664bd72f
Micah Bales <micahbales@gmail.com>
2016-10-30 19:32:36 -0400

    improved fibonacci recursive method

--- challenges/phase-12/factorial/lib/factorial.rb
+++ challenges/phase-12/factorial/lib/factorial.rb
@@ -1,16 +1,13 @@
-def factorial(number, accumulator = 1)
-  if number == 0
-    puts 1
-  elsif number < 2
-    puts accumulator
-  else
-    accumulator *= number
-    factorial(number - 1, accumulator)
-  end
+def factorial(number)
+    if number == 0
+        1
+    else
+        number * factorial(n-1)
+    end
 end
 
-factorial(3) # 6
-factorial(4) # 24
-factorial(5) # 120
+x = factorial(3) # 6
+y = factorial(4) # 24
+z = factorial(5) # 120
 
 # 6 * 5 * 4 * 3 * 2 * 1
